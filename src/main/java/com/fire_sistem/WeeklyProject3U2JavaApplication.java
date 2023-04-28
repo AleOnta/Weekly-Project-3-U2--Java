376976847716192b4591aa467b801462f54be5ca
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-28 14:47:55 +0200

    initialized the project

---
+++ src/main/java/com/fire_sistem/WeeklyProject3U2JavaApplication.java
@@ -0,0 +1,13 @@
+package com.fire_sistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyProject3U2JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyProject3U2JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fire_sistem/WeeklyProject3U2JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fire_sistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyProject3U2JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
