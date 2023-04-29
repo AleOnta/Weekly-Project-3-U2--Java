fce58c468cbdae82b6c164d39eb8a9bec5a6e044
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-29 13:20:51 +0200

    adds probeChecker class as controller for probe detection

---
+++ src/main/java/com/fire_sistem/design_patterns/ProbeChecker.java
@@ -0,0 +1,28 @@
+package com.fire_sistem.design_patterns;
+
+import lombok.Getter;
+
+public class ProbeChecker implements Observer {
+	// static properties of class
+	private static int idCounter = 1;
+	
+	// properties of each instance
+	@Getter
+	private final int id;
+	private I_ControlCentre controlCentre;
+	
+	// constructor
+	public ProbeChecker(I_ControlCentre CC) {
+		this.id = idCounter;
+		this.controlCentre = CC;
+		idCounter++;
+	}
+	
+	// observer interface method
+	@Override
+	public void update(Subject obj) {
+		Probe castedProbe = (Probe) obj;
+		AlarmInfo alarm = new AlarmInfo(castedProbe);
+		controlCentre.detectOngoingFire(alarm);
+	}
+}
