2c79600dae25ee41ffc5e495ff8987f2f47a1c1f
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-29 13:22:05 +0200

    adds project runner and completed the functionality of the project, now will focus on the j-unit testing

---
+++ src/main/java/com/fire_sistem/runner/FireSystemRunner.java
@@ -0,0 +1,57 @@
+package com.fire_sistem.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import com.fire_sistem.design_patterns.ControlCentreProxy;
+import com.fire_sistem.design_patterns.Probe;
+import com.fire_sistem.design_patterns.ProbeChecker;
+import com.fire_sistem.design_patterns.ProbeFactory;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class FireSystemRunner implements ApplicationRunner {
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		log.info("--- Initializing the National Fire System Program ---");
+		log.info("*program correctly initilized*");
+		log.info("");
+		
+		// creating a national fire control station
+		ControlCentreProxy NationalControlCentre = new ControlCentreProxy();
+		
+		// creating a probechecker for each probe that will be created
+		ProbeChecker northernChecker = new ProbeChecker(NationalControlCentre);
+		ProbeChecker westernChecker = new ProbeChecker(NationalControlCentre);
+		ProbeChecker southernChecker = new ProbeChecker(NationalControlCentre);
+		ProbeChecker easternChecker = new ProbeChecker(NationalControlCentre);
+		
+		// creating 4 differents probe through probe factory
+		Probe northernProbe = ProbeFactory.getProbe();
+		Probe westernProbe = ProbeFactory.getProbe();
+		Probe southernProbe = ProbeFactory.getProbe();
+		Probe easternProbe = ProbeFactory.getProbe();
+		
+		// adds observer to each probe
+		northernProbe.subscribeObserver(northernChecker);
+		westernProbe.subscribeObserver(westernChecker);
+		southernProbe.subscribeObserver(southernChecker);
+		easternProbe.subscribeObserver(easternChecker);
+		
+		// performing detection of smokelevel in each probe
+		northernProbe.performDetection();
+		westernProbe.performDetection();
+		southernProbe.performDetection();
+		easternProbe.performDetection();
+		
+		log.info("");
+		log.info("--- Shutting off the National Fire System Program ---");
+		log.info("*program correctly closed*");	
+	}
+
+}
